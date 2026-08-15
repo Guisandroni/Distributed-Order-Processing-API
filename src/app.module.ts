195c1407e1acceea3d43f83fb1640e3d8b2e4cdc
guisandroni <gui.sandroni@gmail.com>
2026-08-15 17:06:27 -0300

    refactor: clean application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
-import { PostsController } from './posts/posts.controller';
 
 @Module({
   imports: [
@@ -19,7 +16,5 @@ import { PostsController } from './posts/posts.controller';
     AuthModule,
     PostsModule,
   ],
-  controllers: [UsersController, PostsController],
-  providers: [UsersService],
 })
 export class AppModule {}
