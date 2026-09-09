a5bc6c9f9059e25483da32c512b03b6072862c0a
guisandroni <gui.sandroni@gmail.com>
2026-09-09 12:16:09 -0300

    chore: add payments constants about dead letter rmq

--- libs/contracts/src/payment-events.ts
+++ libs/contracts/src/payment-events.ts
@@ -1,13 +1,28 @@
+import { DomainEvent } from './domain-event';
+
 export const constants = {
   paymentsClient: 'PAYMENTS_CLIENT',
   paymentsQueue: 'payments_queue',
   paymentRequested: 'payment.requested',
   paymentRequestedEvent: 'PAYMENT_REQUESTED_EVENT',
+
+  paymentsDeadLetterExchange: 'payments.dlx',
+  paymentsDeadLetterQueue: 'payment.requested.dlq',
+  paymentsDeadLetterRoutingKey: 'payment.requested.dead',
 };
 
-export type PaymentRequestedEvent = {
+// export type PaymentRequestedEvent = {
+//   paymentId: number;
+//   orderId: number;
+//   userId: number;
+//   amount: string;
+// };
+
+export type PaymentRequestedPayload = {
   paymentId: number;
   orderId: number;
   userId: number;
   amount: string;
 };
+
+export type PaymentRequestedEvent = DomainEvent<PaymentRequestedPayload>;
